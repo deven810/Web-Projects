f72841fbfabb51081e4c868100f70516e640bff7
Haejin Jo <professionalhaejin@gmail.com>
2018-11-26 05:44:07 +0000

    started preview stuff

--- angular_markdown_editor/angular-blog/src/app/preview/preview.component.ts
+++ angular_markdown_editor/angular-blog/src/app/preview/preview.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BlogService, Post } from "../blog.service";
+import { Parser, HtmlRenderer } from 'commonmark';
 
 @Component({
   selector: 'app-preview',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview.component.css']
 })
 export class PreviewComponent implements OnInit {
-
-  constructor() { }
+  @Input() post: Post;
+  constructor(private route: ActivatedRoute, private blogService: BlogService, private router: Router) {
+    this.route.paramMap.subscribe(() => this.getPost());
+  }
 
   ngOnInit() {
   }
+
+  getPost(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    const user = this.blogService.user;
+    console.log(user)
+    this.post = this.blogService.getPost(this.blogService.user, Number(id))
+    console.log(this.post)
+  }
+
 
 }
